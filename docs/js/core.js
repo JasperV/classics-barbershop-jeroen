6da192e9c88b710adfd2814a0a6a48bf1979e40a
JasperV <1297464+JasperV@users.noreply.github.com>
2019-08-08 10:14:54 +0200

    update

--- docs/js/core.js
+++ docs/js/core.js
@@ -1,41 +1,47 @@
 // Page loadinmg animation
 
-if ((".loader").length) {
-    // show Preloader until the website ist loaded
-    $(window).on('load', function () {
+if (".loader".length) {
+  // show Preloader until the website ist loaded
+  $(window).on("load", function() {
     $(".loader").fadeOut("slow");
-    });
+  });
 }
 
 /* Onpage linkng smooth effect */
 
-$('a[href^="#"]').on('click', function(event) {
+$('a[href^="#"]').on("click", function(event) {
+  console.warn($(this).attr("href"));
 
-    var target = $( $(this).attr('href') );
+  var target = $($(this).attr("href"));
 
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
+  if ($(this).attr("href") === "#carouselExampleCaptions") {
+    return;
+  }
 
+  if (target.length) {
+    event.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: target.offset().top
+      },
+      1000
+    );
+  }
 });
 
 // Sticky Header
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
 
-    if (scroll >= 100) {
-        $(".top-nav").addClass("light-header");
-    } else {
-        $(".top-nav").removeClass("light-header");
-    }
+  if (scroll >= 100) {
+    $(".top-nav").addClass("light-header");
+  } else {
+    $(".top-nav").removeClass("light-header");
+  }
 });
 
 // Year for copy content
-$(function(){
-var theYear = new Date().getFullYear();
-$('#year').html(theYear);
+$(function() {
+  var theYear = new Date().getFullYear();
+  $("#year").html(theYear);
 });
-
